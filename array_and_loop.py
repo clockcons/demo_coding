ac8d7a5070f90d23e4d0456cd257c004a42b1502
clockcons <jevicabs@gmail.com>
2024-10-25 23:09:15 +0800

    change the code because I misunderstood the instructions

--- array_and_loop.py
+++ array_and_loop.py
@@ -8,35 +8,39 @@ def valid_age(age):
         return "invalid"
 
 while True:
-    name = input("Please input name: ")
-    if valid_name(name):
-        print("Invalid Name")
-        continue
-    else:
-        retry = input("Retry? ('Yes' or 'No'): ")
-        if retry == "No":
+    while True:
+        name = input("Please input name: ")
+        if valid_name(name):
+            print("Invalid Name")
+        else:
             break
-        elif retry == "Yes":
+    while True:
+        try:
+            age = int(input("Please input age: "))
+            if valid_age(age) == "invalid":
+                print("Invalid Age")
+                continue
+        except ValueError:
+            print("Age must be a number")
             continue
         else:
-            print("Invalid Input")
-            continue
-
-while True:
-    try:
-        age = int(input("Please input age: "))
-        if valid_age(age) == "invalid":
-            print("Invalid Age")
-            continue
-    except ValueError:
-        print("Age must be a number")
-        continue
-    else:
-        retry = input("Retry? ('Yes' or 'No'): ")
-        if retry == "No":
             break
-        elif retry == "Yes":
-            continue
-        else:
-            print("Invalid Input")
-            continue
+    entry = input("Would you want to input another entry? ('Yes' or 'No'): ")
+    if entry == "Yes":
+        continue
+    elif entry == "No":
+        break
+    else:
+        while True:
+            try:
+                print("Invalid Input")
+                print("Please input either 'Yes' or 'No'")
+                entry = input("Would you want to input another entry? ('Yes' or 'No'): ")
+                if entry == "Yes":
+                    break
+            except entry == "No":
+                while True:    
+                    break
+                break
+            break
+        break
